cd492467826bee33dbd7ca809a585d9ab8886337
abysmal123 <1611316689@qq.com>
2024-05-21 23:49:01 +0800

    Add spring task.

---
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,52 @@
+package com.sky.task;
+
+import com.sky.constant.MessageConstant;
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 定时任务类，定时处理订单状态
+ */
+@Component
+@Slf4j
+public class OrderTask {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @Scheduled(cron = "0 * * * * ?") // 每分钟触发一次
+    public void processTimeoutOrder() {
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-15);
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeTL(Orders.PENDING_PAYMENT, time);
+        if (!CollectionUtils.isEmpty(ordersList)) {
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelReason("支付超时，自动取消");
+                orders.setCancelTime(LocalDateTime.now());
+                orderMapper.update(orders);
+            }
+        }
+    }
+
+    @Scheduled(cron = "0 0 1 * * ?") // 每天凌晨1点触发一次
+    public void processDeliveryOrder() {
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-60);
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeTL(Orders.DELIVERY_IN_PROGRESS, time);
+        if (!CollectionUtils.isEmpty(ordersList)) {
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.COMPLETED);
+                orderMapper.update(orders);
+            }
+        }
+    }
+
+}
